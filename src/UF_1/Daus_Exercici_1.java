007781dcea7c30b44dcdeb7aa79a538853d3f235
Albert.nick <avera@elpuig.xeill.net>
2019-10-08 16:23:00 +0200

    Joc de Daus

--- src/UF_1/Daus_Exercici_1.java
+++ src/UF_1/Daus_Exercici_1.java
@@ -3,7 +3,7 @@ package UF_1;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Daus1 {
+public class Daus_Exercici_1 {
     public static void main(String[] args) {
 
         int [] daus = new int[13];
